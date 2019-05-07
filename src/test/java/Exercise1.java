ad8768f10cf6504090253ba263f71380a6e62cec
marco <marco@marcobehler.com>
2019-05-07 12:00:11 +0200

    2nd version updates

---
+++ src/test/java/Exercise1.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class Exercise1 {
+
+    @Test
+    public void your_code_goes_here() {
+        // fill in your code here
+        System.out.println("#letsgochamp!");
+    }
+}
