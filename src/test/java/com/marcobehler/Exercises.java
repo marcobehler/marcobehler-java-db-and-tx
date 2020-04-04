7ca0e5b5f52da9a1f8f076c73383ee37016d3d39
marco <marco@marcobehler.com>
2020-04-04 22:19:02 +0200

    Rest master branch to empty

---
+++ src/test/java/com/marcobehler/Exercises.java
@@ -0,0 +1,13 @@
+package com.marcobehler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class Exercises {
+
+    @Test
+    public void writeYourTestsInThisPackage() {
+        assertTrue(true);
+    }
+}
